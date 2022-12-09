3e2d369d307a7566794d33d1400b93557419ad24
beyzahsen <beyzaahsenata@gmail.com>
2022-12-09 11:13:57 +0300

    adding some authentication features

---
+++ take_me_out_app/lib/services/auth.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  get user => _auth.currentUser;
+
+  /*
+  //sign up method
+  Future signUp({String email, String password}) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  //sign in method
+  Future signIn({String email, String password}) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  //sign out method
+  Future signOut() async {
+    await _auth.signOut();
+
+    print('signout');
+  }
+  */
+}
