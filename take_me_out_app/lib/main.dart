f5849b6e125308038b4bbb4adf49e801090615a1
beyzahsen <beyzaahsenata@gmail.com>
2022-12-09 12:21:43 +0300

    Revert "adding some authentication features"
    This reverts commit 3e2d369d307a7566794d33d1400b93557419ad24.

--- take_me_out_app/lib/main.dart
+++ take_me_out_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:take_me_out_app/pages/homepage.dart';
-import 'package:take_me_out_app/pages/wrapper.dart';
 
 void main() => runApp(const TakeMeOutApp());
 
@@ -11,10 +10,6 @@ class TakeMeOutApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
-      home: Wrapper(),
-      /*
-      ****bu kisimlari cikarinca bi sey degismedi o yuzden cikardim****
-       
       debugShowCheckedModeBanner: false,
       title: 'Take Me Out',
       theme: ThemeData.light().copyWith(
@@ -29,7 +24,6 @@ class TakeMeOutApp extends StatelessWidget {
         ),
       ),
       home: const HomePage(),
-      */
     );
   }
 }
